38c45878975f75f464709b495db2a7216415c288
Leo Zhu <leozhu333@gmail.com>
2024-04-12 11:43:51 -0400

    first commit

--- src/test/java/com/example/demo/MyTest.java
+++ src/test/java/com/example/demo/MyTest.java
@@ -1,5 +1,17 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MyTest {
-    
+    Controller controller;
+
+    MyTest() {
+        this.controller = new Controller();
+    }
+
+    @Test
+    public void test() {
+        assertEquals("Hello World!", controller.hello());
+    }
 }
